59830c86c27674cabcfedf93b8a638ef611285ca
vespero89 <fabiovesperini@gmail.com>
2017-06-06 12:32:53 +0200

    add esecution time stft

--- Test_features.py
+++ Test_features.py
@@ -1,10 +1,19 @@
 from AudioFeatures import *
+import time
 
-audio_files_path = 'dataset/test_set_paper/trimmed'
+
+audio_files_path = 'dataset/test'
 HOP = 2048
 FTBINS = 4096
-SR = 16000
+SR = 22050
 
+ts0 = time.time()
 test = AudioFeatures(feature='stft', n_fft=FTBINS, hop=HOP, path=audio_files_path, s_rate=SR, extension='.wav', free_disk=False)
-
 test.feat_extract()
+
+
+ts1 = time.time()
+tot_time = (ts1-ts0)
+print (str(tot_time))
+
+
